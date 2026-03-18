fc3c8fdd85c5dfd817fabe1eea1aedb24c31dd0b
hhhirsch <hirsch.hans92@gmail.com>
2026-03-18 23:55:33 +0100

    index.ts aktualisieren

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,7 +1,23 @@
-export * from "./types";
-export * from "./sharedBaseCss";
-export * from "./carouselSlideTemplate";
-export * from "./decisionOutTemplate";
-export * from "./generalPostTemplate";
-export * from "./iqwigOutTemplate";
-export * from "./templateRegistry";
+import { carouselSlideTemplate } from './carouselSlideTemplate';
+import { decisionOutTemplate } from './decisionOutTemplate';
+import { generalPostTemplate } from './generalPostTemplate';
+import { iqwigOutTemplate } from './iqwigOutTemplate';
+import { toWatchTemplate } from './toWatchTemplate';
+
+export const GRAPHIC_TEMPLATES = [
+  toWatchTemplate,
+  decisionOutTemplate,
+  iqwigOutTemplate,
+  carouselSlideTemplate,
+  generalPostTemplate,
+];
+
+export const DEFAULT_TEMPLATE_ID = GRAPHIC_TEMPLATES[0].id;
+
+export * from './types';
+export * from './sharedBaseCss';
+export * from './toWatchTemplate';
+export * from './decisionOutTemplate';
+export * from './iqwigOutTemplate';
+export * from './carouselSlideTemplate';
+export * from './generalPostTemplate';
