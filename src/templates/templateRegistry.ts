55ccdfa785f932380469a718c91be8f61d59df36
hhhirsch <hirsch.hans92@gmail.com>
2026-03-18 23:13:04 +0100

    Add templateRegistry to manage templates

---
+++ src/templates/templateRegistry.ts
@@ -0,0 +1,13 @@
+import { toWatchTemplate } from "./toWatchTemplate";
+import { decisionOutTemplate } from "./decisionOutTemplate";
+import { iqwigOutTemplate } from "./iqwigOutTemplate";
+import { carouselSlideTemplate } from "./carouselSlideTemplate";
+import { generalPostTemplate } from "./generalPostTemplate";
+
+export const templateRegistry = [
+  toWatchTemplate,
+  decisionOutTemplate,
+  iqwigOutTemplate,
+  carouselSlideTemplate,
+  generalPostTemplate
+];
